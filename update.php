208289e4f630046d11b1bc8dc2ba09a0b969c956
Runia87 <ainur.sarybayeva@gmail.com>
2025-03-18 22:44:40 +0600

    Add files via upload

---
+++ update.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'init.php';
+$user= new User();
+$validate=new Validate();
+$validate->check($_POST,['username'=>['required'=>true,'min'=>2]]);
+if(Input::exists()){
+    If(Token::check(Input::get('token'))){
+        If($validate->passed()){
+            $user->update(['username'=>Input::get('username')]);
+            Redirect::to('update.php');
+        }else{
+            foreach($validate->errors() as $error)
+            echo $error.'<br>';
+        }
+        }
+    }
+    var_dump($_POST['token']);
+?>
+<form action="test.php" method="post">
+<div class="field">
+<label for="username">
+Username
+</label>
+<input type="text" name ="username" id ="username" value="<?php echo $user->data()[0]->username;?>">
+</div>
+<div class="field">
+<button type="submit">Submit</button>
+</div>
+<input type="hidden" name ="token" value="<?php echo Token::generate();?>">
+</form>
