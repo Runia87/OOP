edf11fed151e7a241f1c02b5d135634181b70d91
Runia87 <ainur.sarybayeva@gmail.com>
2026-06-27 16:55:26 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+return 
+[
+"database"=>[
+    "database"=>"data",
+    "username"=>"root",
+    "password"=>"",
+    "connection"=>"mysql:host=MySQL-8.0",
+    "charset"=>"utf8"
+
+]
+
+];
+
+?>
